b6e57067a928673ce1cf4a3c8fd223700e7e22ff
fmarca <fredymarca12@gmail.com>
2023-11-01 22:39:00 -0500

    Modificamos index.php

--- practicas/formulario_contacto/index.php
+++ practicas/formulario_contacto/index.php
@@ -15,7 +15,7 @@ if (isset($_POST['submit'])) {
 	if (!empty($correo)) {
 		$correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
 
-		if (!filter_var($correo, FILTER_SANITIZE_EMAIL)){
+		if (!filter_var($correo, FILTER_VALIDATE_EMAIL)){
 			$errores .= 'Por favor ingrese un correo valido <br />';
 		}
 		
@@ -23,8 +23,6 @@ if (isset($_POST['submit'])) {
 		$errores .= 'Por favor ingrese un correo <br />';
 	} 
 
-}
-
-require 'index.view.php';
+	
 
 ?>
