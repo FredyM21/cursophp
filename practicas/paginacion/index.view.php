ed1179c8efc6224d6d9ce1553932b57a01961bd6
FredyM21 <fredymarca12@gmail.com>
2023-11-15 08:33:29 -0500

    agregamos archivos para paginacion

---
+++ practicas/paginacion/index.view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Paginacion</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<div class="contenedor">
+		<h1>Articulos</h1>
+		<section class="articulos">
+			<ul>
+				<li>1.- Lorem, ipsum dolor sit amet consectetur adipisicing.</li>
+				<li>1.- Lorem, ipsum dolor sit amet consectetur adipisicing.</li>
+				<li>1.- Lorem, ipsum dolor sit amet consectetur adipisicing.</li>
+				<li>1.- Lorem, ipsum dolor sit amet consectetur adipisicing.</li>	
+			</ul>
+		</section>
+
+		<section class="paginacion">
+			<ul>
+				<li class="disabled">&laquo;</li>
+				<li class="active"><a href="#">1</a></li>
+				<li><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a href="#">4</a></li>
+				<li><a href="#">&raquo;</a></li>
+			</ul>
+		</section>
+	</div>
+</body>
+</html>
